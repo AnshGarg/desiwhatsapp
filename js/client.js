1af6d870981a5ee53a3fae7b413238fab1092831
AnshGarg <66829088+AnshGarg@users.noreply.github.com>
2020-06-28 10:22:39 +0530

    Folder uploads

---
+++ js/client.js
@@ -0,0 +1,43 @@
+const socket = io("http://localhost:8000")
+
+const form = document.getElementById("send-container");
+const messageinput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+var audio = new Audio("SPLAT.mp3")
+const append = (message, position) => {
+    const messageElement = document.createElement("div");
+    messageElement.innerText = message;
+    messageElement.classList.add("message");
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position == "left"){
+        audio.play();
+    }  
+}
+
+
+const name = prompt("Enter your name to join")
+socket.emit("new-user-joined", name)
+
+
+socket.on("user-joined", name => {
+    append(`${name} joined the chat`, "right")
+});
+
+
+socket.on("receive", data => {
+    append(`${data.name} : ${data.message}`, "left")
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const message = messageinput.value;
+    append(`You : ${message}`, "right")
+    socket.emit("send", message)
+    messageinput.value = ""
+})
+
+
+socket.on("left", data => {
+    append(`${data.name} Left the chat`, "left")
+});
